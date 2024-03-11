ca09170aaaea991b25ed56d6c4c75d0d68f27a6d
22505 <visionstrategie@gmail.com>
2024-03-11 08:27:13 +0000

    first commit

--- consolidation_ressource.py
+++ consolidation_ressource.py
@@ -6,6 +6,7 @@ from flask_restful import Resource
 from flask import request
 
 from dbkeys import supabase
+from utils import PerformGlobal
 from utils_data import  saveDataInJson, dataListGen
 
 consolidationFiliale = {
@@ -31,26 +32,31 @@ class ScriptConsolidation(Resource) :
     lengthDataList = 0
 
     def entiteDataMatrice(self,entite,annee):
-        response = supabase.table('AccesPilotage').select('email, entite, Users(nom)').eq("est_editeur", True).execute().data #test recup email
-        # #print(response)
-        dictionnaire_utilisateurs = {}
-
-        for utilisateur in response:
-            email = utilisateur['email']
-            #print(email)
-            entite = utilisateur['entite']
-            #print(entite)
-            noms = [user['nom'] for user in utilisateur['Users']]
-            dictionnaire_utilisateurs[email] = (noms, entite)
-
-        print(dictionnaire_utilisateurs)
 
         id = f"{entite}_{annee}"
 
         response = supabase.table('DataIndicateur').select("*").eq("id", id).execute().data
         data = response[0]
-
         dataValeurList = data["valeurs"]
+        dataValidationList = data["validations"]
+        
+        #test
+        # responseDatasIndic = supabase.table('Indicateurs').select("formule").execute().data
+        # listTemp = [data["formule"] for data in responseDatasIndic]
+        # dicTemp = responseDatasIndic[0]
+        # print(listTemp)
+
+        #Algo filtre des valeurs que validees
+        listZip = list(zip(dataValeurList, dataValidationList))
+        listResult = []
+        max_length = max(len(sublist) for sublist in dataValeurList)
+
+        for sublist, flags in listZip:
+            temp = [sublist[i] if flags[i] else None for i in range(max_length)]
+            listResult.append(temp)
+        
+        dataValeurList = listResult
+
         
         ScriptConsolidation.lengthDataList = len(dataValeurList)
 
@@ -102,10 +108,97 @@ class ScriptConsolidation(Resource) :
                     children.append(child)
                 result = self.getMatriceConso(children)
                 self.updateAllEntiteDataMatrice(consoEntite,result,annee)
+                self.computePerformEntity(consoEntite, result, annee)
                 print(f"Données consolidées : {consoEntite}")
             except:
                 result = dataListGen()
                 self.updateAllEntiteDataMatrice(consoEntite, result, annee)
+    
+    def computePerformEntity(self, entite, dataValeurList, annee):
+        id = f"{entite}_{annee}"
+        #print(entite)
+        responseListEcart = supabase.table('DataIndicateur').select("ecarts").eq("id", id).execute().data
+        responseDataCible = supabase.table('DataIndicateur').select("cibles").eq("id", id).execute().data
+        responseFormules = supabase.table('Indicateurs').select('formule').execute().data
+        responseTypes = supabase.table('Indicateurs').select('type').execute().data
+        listTypes = [data["type"] for data in responseTypes]
+        #print(listTypes)
+        listFormules = [data["formule"] for data in responseFormules]
+        #print(listFormules)
+        dicTemp = responseListEcart[0]
+        listEcart = dicTemp['ecarts']
+        #print(listEcart)
+        dataTemp = responseDataCible[0]
+        listCible = dataTemp['cibles']
+        #print(listCible)
+
+        indicesListAxes = [16, 46, 206, 221, 280]
+        indicesListEnjeux = [16, 21, 27, 34, 46, 181, 200, 206, 221, 245, 262, 280]
+        listAxes = []
+        listEnjeux = []
+
+        for i, type_value in enumerate(listTypes):
+            if type_value == 'Primaire':
+                formule = listFormules[i]
+                dataCible = listCible[i]
+                dataRealise = dataValeurList[i][0]
+                #print(dataRealise)
+                
+                if formule == "Somme":
+                    if dataCible != None and dataRealise != None:
+                        dataEcart = ((dataCible - dataRealise) / dataCible) * 100
+                        listEcart[i] = dataEcart
+                
+                elif formule == "Dernier mois renseigné":
+                        if dataCible != None and dataRealise != None:
+                            dataEcart = ((dataCible - dataRealise) / dataCible) * 100
+                            listEcart[i] = dataEcart
+                
+                elif formule == "Moyenne":
+                        if dataCible != None and dataRealise != None:
+                            dataEcart = ((dataCible - dataRealise) / dataCible) * 100
+                            listEcart[i] = dataEcart
+        
+        #Calcul de la performance Globale
+        globalPerfData = PerformGlobal(listEcart)
+
+        #decoupage de listEcart pous definir listes des Axes pour performances Axes
+        for i in range(len(indicesListAxes) - 1):
+            axeList = listEcart[indicesListAxes[i]:indicesListAxes[i + 1]]
+            listAxes.append(axeList)
+        for index, item in enumerate(listAxes):
+            l = []
+            count = 0
+            for data in item:
+                if data != None:
+                    l.append(data)
+                    count += 1
+            if l != [] :
+                listAxes[index] = sum(l) / count
+            else:
+                listAxes[index] = None
+
+        #decoupage de listEcart pous definir listes des Enjeux pour performances Enjeux
+        for i in range(len(indicesListEnjeux) - 1):
+            enjeuList = listEcart[indicesListEnjeux[i]:indicesListEnjeux[i + 1]]
+            listEnjeux.append(enjeuList)
+        for index, item in enumerate(listEnjeux):
+            l = []
+            count = 0
+            for data in item:
+                if data != None:
+                    l.append(data)
+                    count += 1
+            if l != [] :
+                listEnjeux[index] = sum(l) / count
+            else:
+                listEnjeux[index] = None
+
+        supabase.table('Performance').update({'performs_piliers': listAxes}).eq('id',id).execute()
+        supabase.table('Performance').update({'performs_enjeux': listEnjeux}).eq('id',id).execute()
+        supabase.table('Performance').update({'performs_global': globalPerfData}).eq('id', id).execute()
+        supabase.table('DataIndicateur').update({"ecarts" : listEcart}).eq('id',id).execute()
+
 
     def post(self):
         args = request.get_json()
