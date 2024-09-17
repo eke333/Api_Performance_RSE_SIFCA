f064f04a55e32bc9a2a3460d08b39d721225001f
eke333 <ehoumanjoel.fr@gmail.com>
2024-09-17 15:50:45 +0000

    app.py est ajusté

--- app.py
+++ app.py
@@ -2,9 +2,10 @@ from flask import Flask
 from flask_cors import CORS
 from flask_restful import Api, Resource
 from routes import main_routes  # Importer le Blueprint
+import os
 
 app = Flask(__name__)
-app.config['MAX_CONTENT_LENGTH'] = 15 * 1024 * 1024  # Limiter la taille des fichiers téléchargés
+app.config['MAX_CONTENT_LENGTH'] = int(os.getenv('MAX_CONTENT_LENGTH', 15 * 1024 * 1024))
 
 api = Api(app)
 
@@ -14,6 +15,7 @@ CORS(app, resources={r"/*": {"origins": "*"}})
 # Enregistrer le Blueprint avec l'application
 app.register_blueprint(main_routes)
 
+
 class HelloWorld(Resource):
     def get(self):
         return {
@@ -21,9 +23,19 @@ class HelloWorld(Resource):
             'infoApi': "Je suis l'API principale pour le projet performance QSE",
         }
 
+
 # Ajouter HelloWorld au routeur API
 api.add_resource(HelloWorld, '/')
 
+# Détection de l'environnement (en Production ou en Développement
+
+if os.getenv('FLASK_ENV') == 'production':
+    # Paramètres de production
+    app.config['DEBUG'] = False
+else:
+    # Paramètres de développement
+    app.config['DEBUG'] = True
+
 if __name__ == '__main__':
     # Pour un serveur local non sécurisé (HTTP)
-    app.run(debug=True, port=5000, host="0.0.0.0")
+    app.run(port=5000, host="0.0.0.0")
